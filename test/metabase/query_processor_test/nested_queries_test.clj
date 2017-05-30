3020f9565f71da94d44c583d0309d55274836cf2
Cam Saül <cammsaul@gmail.com>
2017-05-30 14:15:49 -0700

    Test fixes part 3 :wrench:

--- test/metabase/query_processor_test/nested_queries_test.clj
+++ test/metabase/query_processor_test/nested_queries_test.clj
@@ -11,28 +11,30 @@
             [metabase.test.data.datasets :as datasets]
             [toucan.util.test :as tt]))
 
-(defn- rows+cols {:style/indent 0} [results]
+(defn- rows+cols
+  "Return the `:rows` and relevant parts of `:cols` from the RESULTS.
+   (This is used to keep the output of various tests below focused and manageable.)"
+  {:style/indent 0}
+  [results]
   {:rows (rows results)
    :cols (for [col (get-in results [:data :cols])]
            {:name      (str/lower-case (:name col))
             :base_type (:base_type col)})})
 
-(def ^:private ^:const row-results
-  {:rows [[ 4 1 10.0646 -165.374 "Red Medicine"                 3]
-          [11 2 34.0996 -118.329 "Stout Burgers & Beers"        2]
-          [11 3 34.0406 -118.428 "The Apple Pan"                2]
-          [29 4 33.9997 -118.465 "Wurstküche"                   2]
-          [20 5 34.0778 -118.261 "Brite Spot Family Restaurant" 2]]
-   :cols [{:name "category_id", :base_type :type/Integer}
-          {:name "id",          :base_type :type/Integer}
-          {:name "latitude",    :base_type :type/Float}
-          {:name "longitude",   :base_type :type/Float}
-          {:name "name",        :base_type :type/Text}
-          {:name "price",       :base_type :type/Integer}]})
 
 ;; make sure we can do a basic query with MBQL source-query
 (datasets/expect-with-engines (engines-that-support :nested-queries)
-  row-results
+  {:rows [[1  4 10.0646 -165.374 "Red Medicine"                 3]
+          [2 11 34.0996 -118.329 "Stout Burgers & Beers"        2]
+          [3 11 34.0406 -118.428 "The Apple Pan"                2]
+          [4 29 33.9997 -118.465 "Wurstküche"                   2]
+          [5 20 34.0778 -118.261 "Brite Spot Family Restaurant" 2]]
+   :cols [{:name "id",          :base_type (data/id-field-type)}
+          {:name "category_id", :base_type :type/Integer}
+          {:name "latitude",    :base_type :type/Float}
+          {:name "longitude",   :base_type :type/Float}
+          {:name "name",        :base_type :type/Text}
+          {:name "price",       :base_type :type/Integer}]}
   (rows+cols
     (qp/process-query
       {:database (data/id)
@@ -44,7 +46,17 @@
 
 ;; make sure we can do a basic query with a SQL source-query
 (datasets/expect-with-engines (engines-that-support :nested-queries)
-  row-results
+  {:rows [[ 4 1 10.0646 -165.374 "Red Medicine"                 3]
+          [11 2 34.0996 -118.329 "Stout Burgers & Beers"        2]
+          [11 3 34.0406 -118.428 "The Apple Pan"                2]
+          [29 4 33.9997 -118.465 "Wurstküche"                   2]
+          [20 5 34.0778 -118.261 "Brite Spot Family Restaurant" 2]]
+   :cols [{:name "category_id", :base_type :type/Integer}
+          {:name "id",          :base_type :type/Integer}
+          {:name "latitude",    :base_type :type/Float}
+          {:name "longitude",   :base_type :type/Float}
+          {:name "name",        :base_type :type/Text}
+          {:name "price",       :base_type :type/Integer}]}
   (rows+cols
     (qp/process-query
       {:database (data/id)
@@ -70,7 +82,7 @@
          :type     :query
          :query    {:source-query {:source-table (data/id :venues)}
                     :aggregation  [:count]
-                    :breakout     [[:field-literal :price :type/Integer]]}}))))
+                    :breakout     [[:field-literal (keyword (data/format-name :price)) :type/Integer]]}}))))
 
 ;; make sure we can do a query with breakout and aggregation using a SQL source query
 (datasets/expect-with-engines (engines-that-support :nested-queries)
@@ -82,7 +94,7 @@
          :type     :query
          :query    {:source-query {:native (format "SELECT * FROM %s" (data/format-name "venues"))}
                     :aggregation  [:count]
-                    :breakout     [[:field-literal :price :type/Integer]]}}))))
+                    :breakout     [[:field-literal (keyword (data/format-name :price)) :type/Integer]]}}))))
 
 ;; Make sure we can run queries using source table `card__id` format. This is the format that is actually used by the frontend;
 ;; it gets translated to the normal `source-query` format by middleware. It's provided as a convenience so only minimal changes
@@ -99,7 +111,7 @@
            :type     :query
            :query    {:source-table (str "card__" (u/get-id card))
                       :aggregation  [:count]
-                      :breakout     [[:field-literal :price :type/Integer]]}})))))
+                      :breakout     [[:field-literal (keyword (data/format-name :price)) :type/Integer]]}})))))
 
 ;; make sure `card__id`-style queries work with native source queries as well
 (expect
@@ -114,4 +126,4 @@
            :type     :query
            :query    {:source-table (str "card__" (u/get-id card))
                       :aggregation  [:count]
-                      :breakout     [[:field-literal :price :type/Integer]]}})))))
+                      :breakout     [[:field-literal (keyword (data/format-name :price)) :type/Integer]]}})))))
